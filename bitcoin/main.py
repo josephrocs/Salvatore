8a41887f1e8cafe0e7c650623d803cef4f6e8689
josephrocs <joseph@argiroconsulting.com>
2025-05-23 22:18:35 +1000

    api folder added

--- bitcoin/main.py
+++ bitcoin/main.py
@@ -6,5 +6,6 @@ url = "https://coinmarketcap.com/"
 response = requests.get(url) # Get the HTML content of the page
 soup = BeautifulSoup(response.text, "html.parser") # Parse the HTML content with BeautifulSoup
 dom = etree.HTML(str(soup)) # Convert the BeautifulSoup object to an lxml object for easier XPath queries
-price = dom.xpath('//*[@id="__next"]/div[2]/div[1]/div[2]/div/div[1]/div[5]/div/table/tbody/tr[1]/td[4]/div/span')[0].text
-print("The price of Bitcoin is: " + price, "USD.") # Print the price of Bitcoin
+# The bitcoin price as a XPATH
+price = dom.xpath('//*[@id="__next"]/div[2]/div[1]/div[2]/div/div[1]/div[5]/div/table/tbody/tr[2]/td[4]/div/span')[0].text
+print("The price of Ethereum is: " + price, "USD.") # Print the price of Bitcoin
