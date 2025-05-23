e7f0771eb1f94cdab27f2faefdabe95bde7e77d6
josephrocs <joseph@argiroconsulting.com>
2025-05-23 21:54:46 +1000

    Upload

---
+++ bitcoin/README.md
@@ -0,0 +1,13 @@
+# How to get the price of Bitcoin
+
+## How to run the code?
+
+### Step 1 - Install packages
+```
+pip install -r requirements.txt
+```
+
+### Step 2 - Run the script
+```
+python main.py
+```
---
+++ bitcoin/main.py
@@ -0,0 +1,10 @@
+import requests # This allows us to get data from the internet
+from bs4 import BeautifulSoup # This allows us to parse HTML 
+from lxml import etree # This allows easily extracting data from HTML
+
+url = "https://coinmarketcap.com/"
+response = requests.get(url) # Get the HTML content of the page
+soup = BeautifulSoup(response.text, "html.parser") # Parse the HTML content with BeautifulSoup
+dom = etree.HTML(str(soup)) # Convert the BeautifulSoup object to an lxml object for easier XPath queries
+price = dom.xpath('//*[@id="__next"]/div[2]/div[1]/div[2]/div/div[1]/div[5]/div/table/tbody/tr[1]/td[4]/div/span')[0].text
+print("The price of Bitcoin is: " + price, "USD.") # Print the price of Bitcoin
